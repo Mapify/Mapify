65061e4f50747ed90c9800a7d120f05c54ae3a68
TaylorBett <taylor.bett@hotmail.com>
2014-09-26 21:46:46 +1000

    Functional login prototype
    Committed login page and user profile page that showcases user data
    upon successful login.
    
    Sample data allows you to login with username and password I will
    provide. Also accounts for users that try to log in incorrectly.

---
+++ sql/userTblCreation.sql
@@ -0,0 +1,14 @@
+CREATE TABLE users (
+	userID int,
+	UserName varchar(20),
+	pWord varchar(30),
+	LastName varchar(30),
+	FirstName varchar(30),
+	Address varchar(255),
+	City varchar(255)
+);
+
+INSERT INTO users VALUES(1,'taylorBett', 'taylorbett', 'Bett', 'Taylor','Address1', 'Melbourne');
+INSERT INTO users VALUES(2,'benHare', 'benhare', 'Hare', 'Ben','Address2', 'Sydney');
+INSERT INTO users VALUES(3,'jamesBirt', 'jamesbirt', 'Birt', 'James','Address3', 'Brisbane');
+INSERT INTO users VALUES(4,'pennyDeByl', 'pennydebyl', 'De Byl', 'Penny','Address4', 'Gold Coast');users
---
+++ sqlConnect.php
@@ -0,0 +1,141 @@
+<!doctype html>
+<html class="no-js" lang="en">
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Mapify | User Profile</title>
+    <link rel="stylesheet" href="css/foundation.css" />
+    <script src="js/vendor/modernizr.js"></script>
+  </head>
+  <body>
+    <?php
+
+    //$user = strval($_GET['q']);
+    //$pword = strval($_GET['r']);
+    $clientUser = $_POST["usernameInput"];
+    $clientPWord = $_POST["passwordInput"];
+
+    //PROTECT FROM MYSQL INJECTION
+    $clientUser = stripslashes($clientUser);
+    $clientPWord = stripslashes($clientPWord);
+    $clientUser = mysql_real_escape_string($clientUser);
+    $clientPWord = mysql_real_escape_string($clientPWord);
+    
+    $loginStatus = 0;
+
+    //VARIABLES READY TO SAVE RESULTS
+    $clientFName = "";
+    $clientLName = "";
+    $clientUID = "";
+    $clientAddress = "";
+    $clientCity = "";
+
+    //MYSQL SERVER LOGIN DETAILS
+    $host="131.244.54.3";
+    $port=3306;
+    $username = "13277172";
+    $password = "13277172";
+    $dbname="mappingDB";
+
+    //connection to the database
+    $con = mysql_connect($host, $username, $password, $dbname, $port) 
+      or die("..Unable to connect to MySQL");
+    echo "..Connected to MySQL<br>";
+
+    //select a database to work with
+    $selectedDB = mysql_select_db($dbname,$con) 
+      or die("..Could not select examples");
+    echo "..Selected database: " . $dbname."<br><br>";
+
+    //VERIFY LOGIN
+    $sql="SELECT * FROM users WHERE UserName = '$clientUser' and pWord = '$clientPWord'";
+    $verificationResult=mysql_query($sql);
+
+    if(mysql_num_rows($verificationResult) == 1){
+      echo "..Login successful";
+      $loginStatus = 1;
+    }
+    else{
+      echo "..Incorrect login details";
+      $loginStatus = 0;
+    }
+
+    //IF LOGIN SUCCESS SHOW USER DETAILS
+    if($loginStatus == 1){
+      //execute the SQL query and SELECT appropriate rows FROM appropriate TABLE
+      $result = mysql_query("SELECT * FROM users WHERE UserName = '".$clientUser."'");
+      //$result = mysql_query($con,$sql);
+
+      //$result = mysql_query("SELECT userID,UserName,FirstName,LastName,Address,City FROM users");
+
+      //set up table
+      echo "<table border='1'>
+      <tr>
+      <th>User ID</th>
+      <th>Username</th>
+      <th>First Name</th>
+      <th>Last Name</th>
+      <th>Address</th>
+      <th>City</th>
+      </tr>";
+
+      //fetch the data from the database 
+      while ($row = mysql_fetch_array($result)) {
+        //echo "ID: ".$row{'userID'}.", Name: ".$row{'FirstName'}." ".$row{'LastName'}.", Address: ".$row{'Address'}.", Year: ".$row{'City'}."<br>"; //display the results
+        $clientFName = $row['FirstName'];
+        $clientLName = $row['LastName'];
+        $clientUID = $row['userID'];
+        $clientAddress = $row['Address'];
+        $clientCity = $row['City'];
+        echo "<tr>";
+        echo "<td>" . $row['userID'] . "</td>";
+        echo "<td>" . $row['UserName'] . "</td>";
+        echo "<td>" . $row['FirstName'] . "</td>";
+        echo "<td>" . $row['LastName'] . "</td>";
+        echo "<td>" . $row['Address'] . "</td>";
+        echo "<td>" . $row['City'] . "</td>";
+        echo "</tr>";
+      }
+      echo "</table>";
+
+      //welcome user
+      echo "<h1>Welcome, ".$clientFName." ".$clientLName."</h1><br><br>";
+      echo '<a href="index.html"><button class="small button">Log Out</button></a>';
+    }
+    else{
+      echo '<br><br><div class="row">
+              <div class="large-4 columns">
+                <div class="panel">
+                  <h3>User Login</h3>
+                  <div class="row">
+                    <div class="large-12 medium-12 columns">
+                      <!--LOGIN FORM START--> 
+                        <!--POST METHOD WILL LOAD A NEW PAGE, DataLogin.js below will submit w/out new page load-->
+                        <form id="loginForm" method="POST" action="sqlConnect.php">
+                        <form id="loginForm">
+                          <label>Username</label>
+                          <input id="usernameInput" type="text" placeholder="username" name="usernameInput"/>
+                          <label>Password</label>
+                          <input id="passwordInput" type="password" placeholder="password" name="passwordInput"/>
+                          <input type="submit" class="small button">
+                        </form>
+                      <!--LOGIN FORM END-->
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>';
+    }
+    //close the connection
+    mysql_close($con);
+
+    ?>
+
+<script src="js/vendor/jquery.js"></script>
+    <script src="js/foundation.min.js"></script>
+    <!--<script src="loginSubmit.js"></script>-->
+    <script>
+      $(document).foundation();
+    </script>
+  </body>
+</html>
